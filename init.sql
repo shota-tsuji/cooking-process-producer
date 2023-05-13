1578eb1a93091acf20a8184f3f65d4f2b8d5b2fa
shota-tsuji <shotatuzi@gmail.com>
2023-05-13 09:16:37 +0900

    fix: modify table schema and initial data

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
-insert into recipes (id, title, description) values(0, "カレー", "野菜を煮込んで作る");
-insert into recipes (id, title, description) values(1, "きんぴらごぼう", "人参とごぼうを切って炒める");
+insert into recipes (id, title, description) values("00000000-0000-0000-0000-000000000000", "カレー", "野菜を煮込んで作る");
+insert into recipes (id, title, description) values("11111111-1111-1111-1111-111111111111", "きんぴらごぼう", "人参とごぼうを切って炒める");
 
-insert into resources (id, name, amount) values(0, "人手", 1);
-insert into resources (id, name, amount) values(1, "コンロ(なべ・フライパン)", 2);
+insert into resources (name, amount) values("人手", 1);
+insert into resources (name, amount) values("コンロ(なべ・フライパン)", 2);
 
-insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(0, 0, "野菜を切る", 0, 0, 5);
-insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(1, 0, "煮込む", 1, 1, 25);
+insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), "00000000-0000-0000-0000-000000000000", "野菜を切る", 1, 0, 5);
+insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), "00000000-0000-0000-0000-000000000000", "煮込む", 2, 1, 25);
--- schema.sql
+++ schema.sql
@@ -1,22 +1,22 @@
 USE cooking;
 
 CREATE TABLE IF NOT EXISTS recipes (
-    id INT NOT NULL,
+    id VARCHAR(36) NOT NULL,
     title VARCHAR(140) NOT NULL,
     description VARCHAR(1000),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS resources (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(140) NOT NULL,
     amount INT NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS steps (
-    id INT NOT NULL,
-    recipe_id INT NOT NULL,
+    id VARCHAR(36) NOT NULL,
+    recipe_id VARCHAR(36) NOT NULL,
     description VARCHAR(140) NOT NULL,
     resource_id INT NOT NULL,
     order_number INT UNSIGNED NOT NULL,
