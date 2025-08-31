f326bc73639040a994e9cb830139a8c9109d95e7
shota-tsuji <shotatuzi@gmail.com>
2025-08-31 11:41:09 +0900

    feat: change id to int autoincrement (#4)

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 USE cooking;
 
 CREATE TABLE IF NOT EXISTS recipes (
-    id VARCHAR(36) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(140) NOT NULL,
     description VARCHAR(1000),
     PRIMARY KEY (id)
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS resources (
 
 CREATE TABLE IF NOT EXISTS steps (
     id VARCHAR(36) NOT NULL,
-    recipe_id VARCHAR(36) NOT NULL,
+    recipe_id INT NOT NULL,
     description VARCHAR(140) NOT NULL,
     resource_id BIGINT UNSIGNED NOT NULL,
     order_number INT UNSIGNED NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS processes (
 CREATE TABLE IF NOT EXISTS process_regsitrations (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     process_id BIGINT UNSIGNED NOT NULL,
-    recipe_id VARCHAR(36) NOT NULL,
+    recipe_id INT NOT NULL,
     FOREIGN KEY (process_id)
         REFERENCES processes(id),
     FOREIGN KEY (recipe_id)
