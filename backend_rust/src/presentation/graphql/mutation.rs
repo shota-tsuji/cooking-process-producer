54be6e4cfa3bf31123b1e3e48acaf0da50a8376c
shota-tsuji <shotatuzi@gmail.com>
2025-08-31 20:22:22 +0900

    feat: use ULID

--- backend_rust/src/presentation/graphql/mutation.rs
+++ backend_rust/src/presentation/graphql/mutation.rs
@@ -1,6 +1,6 @@
 use async_graphql::{Context, EmptyMutation, EmptySubscription, ID, Object, Schema};
 use sqlx::mysql::MySqlPool;
-use uuid::Uuid;
+use ulid::Ulid;
 
 use crate::presentation::graphql::object::{CreateProcessInput, CreateRecipeStepInput, CreateResourceInput, ProcessId, RecipeId, Resource, UpdateResourceInput};
 
@@ -23,8 +23,8 @@ impl Mutation {
         _ctx: &Context<'_>,
         recipe_detail_data: CreateRecipeDetailInput,
     ) -> Result<RecipeDetail, String> {
-        let recipe_id = Uuid::new_v4().to_string();
-        println!("{}", recipe_id.clone());
+        let recipe_id = Ulid::new().to_string();
+        println!("ulid: {}", recipe_id.clone());
         let query_result =
             sqlx::query(r#"INSERT INTO recipes (id, title, description) VALUES (?, ?, ?)"#)
                 .bind(recipe_id.clone())
@@ -41,7 +41,7 @@ impl Mutation {
             .steps
             .into_iter()
             .map(|step| Step {
-                id: Uuid::new_v4().to_string(),
+                id: Ulid::new().to_string(),
                 description: step.description,
                 resource_id: step.resource_id,
                 order_number: step.order_number,
@@ -166,7 +166,7 @@ impl Mutation {
             .steps
             .into_iter()
             .map(|step| Step {
-                id: Uuid::new_v4().to_string(),
+                id: Ulid::new().to_string(),
                 description: step.description,
                 resource_id: step.resource_id,
                 order_number: step.order_number,
