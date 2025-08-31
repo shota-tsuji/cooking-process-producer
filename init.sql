9855192775d88a168210025a81b8621d8fa46e87
shota-tsuji <shotatuzi@gmail.com>
2025-08-31 11:24:22 +0900

    feat: change id to int autoincrement

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
-insert into recipes (id, title, description) values("00000000-0000-0000-0000-000000000000", "カレー", "野菜を煮込んで作る");
-insert into recipes (id, title, description) values("11111111-1111-1111-1111-111111111111", "きんぴらごぼう", "人参とごぼうを切って炒める");
+insert into recipes (title, description) values("カレー", "野菜を煮込んで作る");
+insert into recipes (title, description) values("きんぴらごぼう", "人参とごぼうを切って炒める");
 
 insert into resources (name, amount) values("人手", 1);
 insert into resources (name, amount) values("コンロ(なべ・フライパン)", 2);
 
-insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), "00000000-0000-0000-0000-000000000000", "野菜を切る", 1, 0, 5);
-insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), "00000000-0000-0000-0000-000000000000", "煮込む", 2, 1, 25);
-insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), "11111111-1111-1111-1111-111111111111", "野菜を切る", 1, 0, 5);
-insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), "11111111-1111-1111-1111-111111111111", "煮る", 2, 1, 10);
+insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), 1, "野菜を切る", 1, 0, 5);
+insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), 1, "煮込む", 2, 1, 25);
+insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), 2, "野菜を切る", 1, 0, 5);
+insert into steps (id, recipe_id, description, resource_id, order_number, duration) values(UUID(), 2, "煮る", 2, 1, 10);
 
 insert into processes (name) values("process-0");
 
-insert into process_regsitrations (process_id, recipe_id) values(1, "00000000-0000-0000-0000-000000000000");
-insert into process_regsitrations (process_id, recipe_id) values(1, "11111111-1111-1111-1111-111111111111");
+insert into process_regsitrations (process_id, recipe_id) values(1, 1);
+insert into process_regsitrations (process_id, recipe_id) values(1, 2);
