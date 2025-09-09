c7bf390af1a095ad5ca6d9794a25715fbfbd963c
shota-tsuji <shotatuzi@gmail.com>
2025-09-09 20:03:40 +0900

    feat: Create Tables

--- backend_rust/migration/src/m20220101_000001_create_table.rs
+++ backend_rust/migration/src/m20220101_000001_create_table.rs
@@ -6,36 +6,156 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
+        // recipes
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Recipes::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(ColumnDef::new(Recipes::Id).string_len(36).not_null().primary_key())
+                    .col(ColumnDef::new(Recipes::Title).string_len(140).not_null())
+                    .col(ColumnDef::new(Recipes::Description).string_len(1000))
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        // resources
+        manager
+            .create_table(
+                Table::create()
+                    .table(Resources::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Resources::Id).big_unsigned().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(Resources::Name).string_len(140).not_null())
+                    .col(ColumnDef::new(Resources::Amount).integer().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // processes
+        manager
+            .create_table(
+                Table::create()
+                    .table(Processes::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Processes::Id).big_unsigned().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(Processes::Name).string_len(140).not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // steps
+        manager
+            .create_table(
+                Table::create()
+                    .table(Steps::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Steps::Id).string_len(36).not_null().primary_key())
+                    .col(ColumnDef::new(Steps::RecipeId).string_len(36).not_null())
+                    .col(ColumnDef::new(Steps::Description).string_len(140).not_null())
+                    .col(ColumnDef::new(Steps::ResourceId).big_unsigned().not_null())
+                    .col(ColumnDef::new(Steps::OrderNumber).integer().unsigned().not_null())
+                    .col(ColumnDef::new(Steps::Duration).integer().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(Steps::Table, Steps::RecipeId)
+                            .to(Recipes::Table, Recipes::Id),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(Steps::Table, Steps::ResourceId)
+                            .to(Resources::Table, Resources::Id),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        // process_regsitrations
+        manager
+            .create_table(
+                Table::create()
+                    .table(ProcessRegsitrations::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(ProcessRegsitrations::Id).big_unsigned().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(ProcessRegsitrations::ProcessId).big_unsigned().not_null())
+                    .col(ColumnDef::new(ProcessRegsitrations::RecipeId).string_len(36).not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(ProcessRegsitrations::Table, ProcessRegsitrations::ProcessId)
+                            .to(Processes::Table, Processes::Id),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(ProcessRegsitrations::Table, ProcessRegsitrations::RecipeId)
+                            .to(Recipes::Table, Recipes::Id),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
+        // Drop in reverse order due to foreign key constraints
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+            .drop_table(Table::drop().table(ProcessRegsitrations::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Steps::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Processes::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Resources::Table).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(Recipes::Table).to_owned())
+            .await?;
+        Ok(())
     }
 }
 
+// Table idents
 #[derive(DeriveIden)]
-enum Post {
+enum Recipes {
     Table,
     Id,
     Title,
-    Text,
+    Description,
+}
+
+#[derive(DeriveIden)]
+enum Resources {
+    Table,
+    Id,
+    Name,
+    Amount,
+}
+
+#[derive(DeriveIden)]
+enum Steps {
+    Table,
+    Id,
+    RecipeId,
+    Description,
+    ResourceId,
+    OrderNumber,
+    Duration,
+}
+
+#[derive(DeriveIden)]
+enum Processes {
+    Table,
+    Id,
+    Name,
+}
+
+#[derive(DeriveIden)]
+enum ProcessRegsitrations {
+    Table,
+    Id,
+    ProcessId,
+    RecipeId,
 }
