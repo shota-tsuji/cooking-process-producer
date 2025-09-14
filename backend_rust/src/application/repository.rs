4d8be0464c0a229d878e47da8b9ccbdada9e2cdf
shota-tsuji <shotatuzi@gmail.com>
2025-09-15 08:26:46 +0900

    refactor: re-export repository traits

--- backend_rust/src/application/repository.rs
+++ backend_rust/src/application/repository.rs
@@ -1,3 +1,7 @@
 pub mod process_repository;
 pub mod recipe_repository;
 pub mod resource_repository;
+
+pub use process_repository::ProcessRepository;
+pub use recipe_repository::RecipeRepository;
+pub use resource_repository::ResourceRepository;
