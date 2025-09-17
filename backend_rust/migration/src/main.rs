f484a5e4cff366fd064b67978603fcfa620d43a9
shota-tsuji <shotatuzi@gmail.com>
2025-09-17 19:12:10 +0900

    test: migration as cli

--- backend_rust/migration/src/main.rs
+++ backend_rust/migration/src/main.rs
@@ -1,6 +1,21 @@
 use sea_orm_migration::prelude::*;
+use sea_orm_migration::cli;
+use sea_orm_cli::MigrateSubcommands;
 
 #[async_std::main]
 async fn main() {
-    cli::run_cli(migration::Migrator).await;
+    //cli::run_cli(migration::Migrator).await;
+
+    // Run migration with Fresh command
+    let db = sea_orm::Database::connect(std::env::var("DATABASE_URL").expect("DATABASE_URL not set"))
+        .await
+        .expect("Failed to connect to database");
+    cli::run_migrate(
+        migration::Migrator,
+        &db,
+        Some(MigrateSubcommands::Fresh),
+        false,
+    )
+    .await
+    .expect("Migration failed");
 }
